6159d50acc4863f43d7298b5b3044dd2d92d69f9
Lucas Cruz <lucas.cruz@linkedby.com.br>
2023-10-16 21:05:12 -0300

    feat: add model funcionário
    adicionado classe funcionário em model

---
+++ gestaoPessoas/src/main/java/com/example/gestaoPessoas/model/Funcionario.java
@@ -0,0 +1,33 @@
+package com.example.gestaoPessoas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+}
